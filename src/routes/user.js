65963da76f84bc5e00c1238ce6df2ea6a00bfb3a
khs6226 <khsun6226@naver.com>
2021-04-30 02:26:45 -0700

    add database connection to posting route

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const router = express.Router();
+const dbConnection = require('../dbConnection');
+const dbQuery = require('../dbQuery');
 const { auth, requiresAuth } = require('express-openid-connect');
 
 router.get('/profile', requiresAuth(), function (req, res) {
@@ -16,7 +18,22 @@ router.get('/posting', requiresAuth(), (req, res) => {
   let authenticated = req.oidc.isAuthenticated();
   let user = req.oidc.user;
   if (authenticated) {
-    res.render('posting', { user: user });
+    dbConnection.getConnection((err, dbConnection) => {
+      if(err) {
+        console.log('error connecting to MySQL');
+        console.log(err)
+      } else {
+        dbQuery.getPost((err, result) => {
+          if(err) {
+            console.log('error reading from mysql');
+            console.log(err);
+          } else {
+            res.render('posting', { user: user, post: result });
+          }
+        });
+        dbConnection.release();
+      }
+    })
   } else {
     res.redirect('/');
   }
