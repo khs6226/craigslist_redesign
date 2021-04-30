081488d749914fc0b28dd8eea6a3ab428962b270
matteo <matteo@pop-os.localdomain>
2021-04-30 11:02:27 -0700

    navbar bug fixed

--- src/public/js/menu.js
+++ src/public/js/menu.js
@@ -1,21 +1,29 @@
 
 $('.menu-icon').click((e) => {
-  let isIndex = $('body').hasClass('home');
-  console.log('click');
-
-  if(isIndex) {
-    $('.nav-menu-container').toggleClass('show-menu', 500);
-    if ($('.menu-icon').attr('src') == './img/menu.svg') {
-      $('.menu-icon').attr('src', './img/close.svg');
-    } else {
-      $('.menu-icon').attr('src', './img/menu.svg');
-    }
+  // let isIndex = $('body').hasClass('home');
+  
+  $('.nav-menu-container').toggleClass('show-menu', 500);
+  if ($('.menu-icon').attr('src') == '/img/menu.svg') {
+    $('.menu-icon').attr('src', '/img/close.svg');
   } else {
-    $('.nav-menu-container').toggleClass('show-menu', 500);
-    if ($('.menu-icon').attr('src') == '/img/menu.svg') {
-      $('.menu-icon').attr('src', '/img/close.svg');
-    } else {
-      $('.menu-icon').attr('src', '/img/menu.svg');
-    }
+    $('.menu-icon').attr('src', '/img/menu.svg');
   }
+  
 })
+
+
+// if(isIndex) {
+//   $('.nav-menu-container').toggleClass('show-menu', 500);
+//   if ($('.menu-icon').attr('src') == '/img/menu.svg') {
+//     $('.menu-icon').attr('src', '/img/close.svg');
+//   } else {
+//     $('.menu-icon').attr('src', '/img/menu.svg');
+//   }
+// } else {
+//   $('.nav-menu-container').toggleClass('show-menu', 500);
+//   if ($('.menu-icon').attr('src') == '/img/menu.svg') {
+//     $('.menu-icon').attr('src', '/img/close.svg');
+//   } else {
+//     $('.menu-icon').attr('src', '/img/menu.svg');
+//   }
+// }
