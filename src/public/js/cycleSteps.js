f337f28ec22f5edfc124f79e43178e49f3fbd655
matteomiceli <omatteomiceli@gmail.com>
2021-05-04 13:53:30 -0700

    working on frontend cycling

---
+++ src/public/js/cycleSteps.js
@@ -0,0 +1,13 @@
+const stepOne = $('#step-1');
+const stepTwo = $('#step-2');
+const stepThree = $('#step-3');
+
+const arrowLeft = $('.arrow-left');
+const arrowRight = $('.arrow-right');
+
+
+
+let steps = [stepOne, stepTwo, stepThree];
+arrowRight.on('click', (e) => {
+
+})
