bcd8c397dcf39b889a3b5459c5e6de8be1c98751
matteomiceli <omatteomiceli@gmail.com>
2021-05-04 19:24:44 -0700

    dynamic form for making new posts

--- src/public/js/cycleSteps.js
+++ src/public/js/cycleSteps.js
@@ -4,14 +4,46 @@ const stepThree = $('#step-3');
 
 const back = $('.button-back');
 const next = $('.button-next');
+const submit = $('#new-post-submit')
 
 stepTwo.css('display', 'none');
 stepThree.css('display', 'none');
 
+
 let steps = [stepOne, stepTwo, stepThree];
-next.on('click', (e) => {
-    for (let i = 0; i < steps.length; i++) {
-        steps[0].css('display', 'none');
-        steps[i].css('display', 'none'); 
+let i = 0;
+
+// checks the current step and applies buttons dynamically
+function checkStep() {
+    if (i == 2) {
+        next.css('display', 'none');
+        submit.css('display', 'block')
+    } else {
+        next.css('display', 'block');
+        submit.css('display', 'none')
     }
+    
+    if (i == 0) {
+        back.css('visibility', 'hidden');
+    } else {
+        back.css('visibility', 'visible');
+    }
+}
+
+checkStep();
+
+next.on('click', (e) => {
+    i++;
+    steps[i-1].css('display', 'none');
+    steps[i].css('display', 'flex');
+
+    checkStep();
+});
+
+back.on('click', (e) => {
+    i-- 
+    steps[i+1].css('display', 'none');
+    steps[i].css('display', 'flex');
+
+    checkStep();
 })
