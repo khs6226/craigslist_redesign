6f91ebc179b547ecb28e6f43cb66af13bec17a60
matteomiceli <omatteomiceli@gmail.com>
2021-05-06 13:56:07 -0700

    \n breaks from database converted to html compatible <br>

--- src/models/searchQuery.js
+++ src/models/searchQuery.js
@@ -3,6 +3,7 @@ const db = require('../dbConnection');
 function searchQuery(queryString, cb) {
     let searchParam = `%${queryString}%`
 
+    // returns post review data 
     let sqlQuery = `SELECT post_id, title, description, price FROM post WHERE title LIKE :queryString OR description LIKE :queryString OR category LIKE :queryString;;`;
     let params = { queryString: searchParam }
     db.query(sqlQuery, params, (err, results) => {
