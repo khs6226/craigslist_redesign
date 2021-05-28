acc1bbf671ea9bd51fc816f2377a2a4b44180d52
khs6226 <khsun6226@naver.com>
2021-05-27 21:52:29 -0700

    create extra pages

--- src/routes/other.js
+++ src/routes/other.js
@@ -7,9 +7,24 @@ const { getFileStream } = require('../models/S3');
 const { auth, requiresAuth } = require('express-openid-connect');
 const { categorySearch } = require('../models/searchModel');
 
-router.get('/', (req, res) => {
+router.get('/contact', (req, res) => {
     let user = req.oidc.user;
     res.render('contact-us', { user: user })
+})
+
+router.get('/privacy', (req, res) => {
+  let user = req.oidc.user;
+  res.render('privacy', { user: user })
+})
+
+router.get('/faq', (req, res) => {
+  let user = req.oidc.user;
+  res.render('faq', { user: user })
+})
+
+router.get('/terms', (req, res) => {
+  let user = req.oidc.user;
+  res.render('terms', { user: user })
 })
 
 module.exports = router;
